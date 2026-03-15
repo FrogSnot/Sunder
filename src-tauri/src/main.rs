ce9e9f10db2f7baebacfbe2059fc1ce163f5ea3b
FrogSnot <ppop10017@gmail.com>
2026-03-15 23:09:06 +0100

    added rpm package and added compatibility flag if using AppImage

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    if std::env::var_os("APPIMAGE").is_some() {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
+
     sunder_lib::run()
 }
